837a66b40170d74ae39f92c919cfceaa0085612a
Snakejac <97687366+snakejac5@users.noreply.github.com>
2025-03-14 09:15:00 -0600

    _

--- planet.py
+++ planet.py
@@ -1,27 +1,71 @@
-name_to_location = {
-    
+import openpyxl
+
+attr_to_prop = {
+    "name": "name",
+    "government": "government",
+    "description": "description",
+    "type": "type",
+    "surface": "surface",
+    "color": "color",
+    "orbital_distance": "orbitaldistance",
+    "eccentricity": "eccentricity",
+    "argument_of_periapsis": "argumentofperiapsis",
+    "orbital_position": "orbitalposition",
+    "orbital_period": "orbitalperiod",
+    "rotational_period": "rotationalperiod",
+    "mass": "mass",
+    "radius": "radius",
+    "density": "density",
+    "inclination": "inclination",
+    "temperature": "temperature",
 }
+prop_locations = {
+    "name": "BK",
+    "government": "BZ",
+    "description": "BZ",
+    "type": "BL",
+    "surface": "BM",
+    "color": "",  # needs coding
+    "orbitaldistance": "T",
+    "eccentricity": "Z",
+    "argumentofperiapsis": "BN",
+    "orbitalposition": "",  # needs coding
+    "orbitalperiod": "V",
+    "rotationalperiod": "U",
+    "mass": "L",
+    "radius": "P",
+    "density": "S",
+    "inclination": "BO",
+    "temperature": "AI",
+}
+
+suffixes = "abcdefghijklmnopqrstuvwxyz"
 
 
 class Planet:
-    def __init__(self, name=None, government=None, description=None, type_=None, surface=None, color=None,
-                 orbital_distance=None, eccentricity=None, argument_of_periapsis=None, orbital_position=None,
-                 orbital_period=None, rotational_period=None, mass=None, radius=None, density=None, inclination=None,
-                 temperature=None):
-        self.name = name
-        self.government = government
-        self.description = description
-        self.type_ = type_
-        self.surface = surface
-        self.color = color
-        self.orbital_distance = orbital_distance
-        self.eccentricity = eccentricity
-        self.argument_of_periapsis = argument_of_periapsis
-        self.orbital_position = orbital_position
-        self.orbital_period = orbital_period
-        self.rotational_period = rotational_period
-        self.mass = mass
-        self.radius = radius
-        self.density = density
-        self.inclination = inclination
-        self.temperature = temperature
+    def __init__(self, suffix, **kwargs):
+        self.row_index = suffixes.index(suffix) + 5  # Where the row of info in the spreadsheet is
+        for attr, val, in kwargs.items():
+            self.__setattr__(attr, val)
+    
+    def fill_attr(self, spreadsheet: openpyxl.Workbook):
+        for prop, location in prop_locations.items():
+            try:
+                self.__setattr__(prop, spreadsheet["System Builder"][location + str(self.row_index)].value)
+            except AttributeError:
+                pass
+
+
+if __name__ == '__main__':
+    planets = [
+        Planet("b"),
+        Planet("c"),
+        Planet("d"),
+        Planet("e"),
+        Planet("f"),
+        Planet("g"),
+        Planet("h"),
+    ]
+    for planet in planets:
+        planet.fill_attr(openpyxl.open("worldbuilding spreadsheet 33 sextantis.xlsx", data_only=True))
+        print("\n".join(f"{key}: {val}" for key, val in planet.__dict__.items()))
