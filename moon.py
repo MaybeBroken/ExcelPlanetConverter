5a9acff94d478bdd9ff2226f258039a63576ee13
Snakejac <97687366+Snakejac5@users.noreply.github.com>
2025-03-19 13:55:21 -0600

    GUI Modifications

--- moon.py
+++ moon.py
@@ -1,3 +1,145 @@
+import random
+
+import openpyxl
+from lxml import etree
+
+props = [
+    "name",
+    "government",
+    "type",
+    "surface",
+    "color",
+    "orbitaldistance",
+    "eccentricity",
+    "argumentofperiapsis",
+    "orbitalposition",
+    "orbitalperiod",
+    "rotationalperiod",
+    "mass",
+    "radius",
+    "density",
+    "inclination",
+    "temperature",
+]
+prop_locations = {
+    "name": "BK",
+    "government": "BZ",
+    "description": "BZ",
+    "type": "BL",
+    "surface": "BM",
+    # "color": "",  # location coded elsewhere
+    "orbitaldistance": "T",
+    "eccentricity": "Z",
+    "argumentofperiapsis": "BN",
+    #     "orbitalposition": "",  # location coded elsewhere
+    "orbitalperiod": "V",
+    "rotationalperiod": "U",
+    "mass": "L",
+    "radius": "P",
+    "density": "S",
+    "inclination": "BO",
+    "temperature": "AI",
+}
+hab_to_color = {
+    "Roche": "#808080FF",
+    "Melting": "#FF0000FF",
+    "Hot": "#FF6600FF",
+    "Low-hum": "#FFC000FF",
+    "Slow-rot": "#FFFF00FF",
+    "Opt in": "#92D050FF",
+    "Ideal": "#00B050FF",
+    "Opt out": "#00FFCCFF",
+    "Dry/H2": "#00B0F0FF",
+    "High-H2": "#2F75B5FF",
+    "Frozen": "#9BC2E6FF",
+}
+
+suffixes = "bcdefghijklmnopqrstuvwxyz"
+
+
+def hex_to_rgb(hex_color) -> tuple[str, ...]:
+    hex_color = hex_color.lstrip("#")
+    return tuple(str(int(hex_color[i:i + 2], 16)) for i in (0, 2, 4, 6))
+
+
 class Moon:
-    def __init__(self):
-        pass
+    def __init__(self, suffix, **kwargs):
+        self.row_index = suffixes.index(suffix) + 6  # Where the row of info in the spreadsheet is
+        for attr, val, in kwargs.items():
+            self.__setattr__(attr, val)
+    
+    def fill_attr(self, spreadsheet) -> None:
+        try:
+            self.__setattr__("color", " ".join(list(hex_to_rgb(hab_to_color[spreadsheet["System Builder"]["AJ" + str(self.row_index)].value]))))
+        except KeyError:
+            pass
+        self.__setattr__("orbitalposition", random.randint(0, 3590) / 10)
+        for prop, location in prop_locations.items():
+            try:
+                value = spreadsheet["System Builder"][location + str(self.row_index)].value
+                if isinstance(value, float):
+                    self.__setattr__(prop, (round(float(value), 2)))
+                else:
+                    self.__setattr__(prop, value if value not in [None, "#NUM!", "#DIV/0!"] else "")
+            except AttributeError as e:
+                print(e)
+                self.__setattr__(prop, "")
+    
+    def get_xml_repr(self) -> str:
+        """
+            <moon
+                name= "Mercury"
+                model= "Moon"
+                type= "Rock"
+                surface= "mercury.jpg"
+                color= "255 160 0 255"
+                orbitaldistance= "0.38"
+                orbitalposition= "190"
+                orbitalperiod= "0.24"
+                rotationperiod= "58.666"
+                mass= "455.8115"
+                radius= "2439"
+                density= "5.43"
+                axistilt= "0"
+            />
+        """
+        s = "<moon model='Moon' "
+        for prop in props:
+            try:
+                value = self.__getattribute__(prop)
+            except AttributeError:
+                continue
+            if value is not None and value not in ("#NUM!", "#DIV/0!"):
+                if str(value).replace(".", "").isnumeric():
+                    if int(value) != float(value):
+                        value = round(float(value), 2)
+                s += f"{prop}={str(value).__repr__()} "
+        s += ">\n<description>"
+        if (description := self.__getattribute__("description")) is not None:
+            s += description
+        s += "</description>\n"
+        s += "<atmosphere />\n"  # to be implemented
+        s += "</moon>\n"
+        return s
+
+
+if __name__ == '__main__':
+    moons = [
+        Moon("b"),
+        Moon("c"),
+        Moon("d"),
+        Moon("e"),
+        Moon("f"),
+        Moon("g"),
+        Moon("h"),
+        Moon("i"),
+        Moon("j"),
+    ]
+    print("building")
+    spreadsheet = openpyxl.open("worldbuilding spreadsheet 33 sextantis.xlsx", data_only=True)
+    for moon in moons:
+        moon.fill_attr(spreadsheet)
+    print("done")
+    for moon in moons:
+        # print(moon.get_xml_repr())
+        print(etree.tostring(etree.fromstring(moon.get_xml_repr()), pretty_print=True, encoding=str))
--- planet_frame.py
+++ planet_frame.py
@@ -9,34 +9,53 @@ from planet import Planet
 limit_none = lambda v: True
 limit_integer = lambda v: isinstance(v, int)
 limit_float = lambda v: isinstance(v, float) or isinstance(v, int)
-limit_color = lambda v: isinstance(v, int) and 0 <= v < 256
+def limit_color(color_str):
+    for val in color_str.split(" "):
+        if not (val.isdigit() and 0 <= int(val) < 256):
+            return False
+    return True
+
+
+
 
 ctk.set_appearance_mode("dark")
+
+attr_to_display = {
+    "name": "Name",
+    "government": "Government",
+    "type": "Type",
+    "surface": "Surface",
+    "color": "Color",
+    "orbitaldistance": "Orbital Distance",
+    "eccentricity": "Eccentricity",
+    "argumentofperiapsis": "Argument of Periapsis",
+    "orbitalposition": "Orbital Position",
+    "orbitalperiod": "Orbital Period",
+    "rotationalperiod": "Rotational Period",
+    "mass": "Mass",
+    "radius": "Radius",
+    "density": "Density",
+    "inclination": "Inclination",
+    "temperature": "Temperature",
+}
 
 value_limits = {
     "name": (limit_none, None),
     "government": (limit_none, None),
-    "type": (limit_none, lambda m: ctk.CTkOptionMenu(m, values=[
-        "Rock",
-        "Ice",
-        "Gas_Giant",
-        "Volcanic",
-        "Water",
-        "Terrestrial",
-    ])),
+    "type": (limit_none, lambda m, **kwargs: ctk.CTkOptionMenu(m, values=["Rock", "Ice", "Gas_Giant", "Volcanic", "Water", "Terrestrial", ])),
     "surface": (limit_none, None),
-    "color": ((limit_color,) * 4, None),
-    "orbitaldistance": (limit_float, None),
+    "color": (limit_color, None, "RGBA"),
+    "orbitaldistance": (limit_float, None, "°"),
     "eccentricity": (limit_float, None),
-    "argumentofperiapsis": (limit_float, None),
-    "orbitalposition": (limit_float, None),
+    "argumentofperiapsis": (limit_float, None, "°"),
+    "orbitalposition": (limit_float, None, "°"),
     "orbitalperiod": (limit_float, None),
     "rotationalperiod": (limit_float, None),
-    "mass": (limit_float, None),
+    "mass": (limit_float, None, "und"),
     "radius": (limit_float, None),
-    "density": (limit_float, None),
-    "inclination": (limit_float, None),
-    "temperature": (limit_float, None),
+    "density": (limit_float, None, "(g/cm³)"),
+    "inclination": (limit_float, None, "°"),
+    "temperature": (limit_float, None, "K"),
 }
 
 
@@ -44,10 +63,10 @@ class PlanetFrame(ctk.CTkFrame):
     systems = {}
     
     def __init__(self, master, planet_obj: Planet, system):
-        super().__init__(master)
+        super().__init__(master, fg_color=("gray92", "gray14"), corner_radius=8, border_width=4, border_color=("gray92", "gray14"))
         self.columnconfigure(0, weight=1)
-        self.title = ctk.CTkButton(self, text=planet_obj.__getattribute__("name"), command=self.toggle_expansion)
-        self.title.grid(sticky="EW", columnspan=2)
+        self.title = ctk.CTkButton(self, text=planet_obj.__getattribute__("name") or " ", command=self.toggle_expansion)
+        self.title.grid(sticky="EW", columnspan=3)
         self.expanded = False
         self.grid_propagate(False)
         self.update_idletasks()
@@ -59,22 +78,27 @@ class PlanetFrame(ctk.CTkFrame):
             PlanetFrame.systems[system].append(self)
         else:
             PlanetFrame.systems[system] = [self]
-            
+        
         for attr, attr_attr in value_limits.items():
             try:
                 __value = planet_obj.__getattribute__(attr)
             except AttributeError:
                 __value = ""
             self.__setattr__(f"planet.{attr}", __value)
-            self.__setattr__(f"planet.{attr}.label", ctk.CTkLabel(self, text=attr))
-            self.__setattr__(f"planet.{attr}.field", (attr_attr[1] or ctk.CTkEntry)(self))
-            self.__getattribute__(f"planet.{attr}.label").grid()
+            self.__setattr__(f"planet.{attr}.label", ctk.CTkLabel(self, text=f"{attr_to_display[attr]}:"))
+            try:
+                self.__setattr__(f"planet.{attr}.label_after", ctk.CTkLabel(self, text=f"{attr_attr[2] or ' '}"))
+            except IndexError:
+                self.__setattr__(f"planet.{attr}.label_after", ctk.CTkLabel(self, text=f""))
+            self.__setattr__(f"planet.{attr}.field", (attr_attr[1] or ctk.CTkEntry)(self, border_color=("gray92", "gray14")))
+            self.__getattribute__(f"planet.{attr}.label").grid(sticky="E", padx=4, pady=1)
             field = self.__getattribute__(f"planet.{attr}.field")
             if isinstance(field, ctk.CTkEntry):
                 field.insert(0, str(__value))
             else:
                 field.set(__value)
-            field.grid(row=self.__getattribute__(f"planet.{attr}.label").grid_info()["row"], column=1)
+            field.grid(row=self.__getattribute__(f"planet.{attr}.label").grid_info()["row"], column=1, padx=(4, 0), sticky="W")
+            self.__getattribute__(f"planet.{attr}.label_after").grid(row=self.__getattribute__(f"planet.{attr}.label").grid_info()["row"], column=2, sticky="W", padx=(0, 4))
             self.__setattr__(f"planet.{attr}.limit", attr_attr[0])
         self.grid_propagate(False)
     
@@ -85,12 +109,12 @@ class PlanetFrame(ctk.CTkFrame):
             self.grid_propagate(True)
         else:
             self.grid_propagate(False)
-            self.configure(height=self.title.winfo_height(), width=258)
+            self.configure(height=self.title.winfo_height(), width=258, require_redraw=True)
         self.expanded = not self.expanded
     
     def de_expand(self):
         self.grid_propagate(False)
-        self.configure(height=self.title.winfo_height(), width=258)
+        self.configure(height=self.title.winfo_height(), width=258, require_redraw=True)
         self.expanded = False
 
 
@@ -104,8 +128,8 @@ if __name__ == '__main__':
         Planet("f"),
         Planet("g"),
         Planet("h"),
-        Planet("i"),
-        Planet("j"),
+        # Planet("i"),
+        # Planet("j"),
     ]
     print("building")
     spreadsheet = openpyxl.open("worldbuilding spreadsheet 33 sextantis.xlsx", data_only=True)
@@ -113,9 +137,10 @@ if __name__ == '__main__':
         planet.fill_attr(spreadsheet)
     print("done")
     root = ctk.CTk()
+    root.title("Planet Display Test")
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=1)
-    container = ctk.CTkScrollableFrame(root)
+    container = ctk.CTkScrollableFrame(root, width=350, height=400)
     container.grid(sticky="NESW")
     for planet in planets:
         # print(planet.get_xml_repr())
