25a979858aef2dcaa08320c186a973d9bf1a9351
Snakejac <97687366+Snakejac5@users.noreply.github.com>
2025-03-24 14:35:50 -0600

    GUI Modifications

--- system_toplevel.py
+++ system_toplevel.py
@@ -2,11 +2,11 @@ import customtkinter as ctk
 import tkinter as tk
 
 
-class MainWindow(ctk.CTk):
+class SystemWindow(ctk.CTkToplevel):
     def __init__(self):
         super().__init__()
-        
-        
+
+
 if __name__ == '__main__':
     ctk.set_appearance_mode("dark")
-    MainWindow().mainloop()
+    SystemWindow().mainloop()
