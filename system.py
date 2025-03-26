4802a9728db0cf769713c14228de4faa17eb6737
Snakejac <97687366+Snakejac5@users.noreply.github.com>
2025-03-26 15:31:06 -0600

    GUI Modifications

--- system.py
+++ system.py
@@ -1,3 +1,31 @@
+
+
+props = [
+    "name",
+    "faction",
+    "class",
+    "radius",
+    "luminosity",
+    "position",
+    "color",
+    "ambient",
+    "pedia",
+]
+
+prop_locations = {
+    "name": "",
+    "faction": "",
+    "class": "",
+    "radius": "E12",
+    "luminosity": "E11",
+    "position": "",
+    "color": "",
+    "ambient": "",
+    "pedia": "",
+}
+
+
 class System:
     def __init__(self):
         pass
+    
